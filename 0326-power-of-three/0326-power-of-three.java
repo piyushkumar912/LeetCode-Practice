a39a2d455751bac173db6dd90263a72f64637ec8
PIYUSH KUMAR <piyushrana7766@gmail.com>
2026-06-11 23:55:47 +0530

    Time: 8 ms (90.96%), Space: 46.1 MB (24.23%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,5 +1,21 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        
+        if(n<=0){
+            return false;
+        }
+        while(n!=1){
+            if(n % 3 == 0){
+                n = n / 3;
+            }
+            else{
+                    return false;
+                }
+        }
+        return true;
     }
 }
+
+
+//Agar koi number power of 3 hai, to usse baar-baar 3 se divide karne par finally 1 mil jayega.
+//Time Complexity: O(log₃ n)
+//Space Complexity: O(1)
