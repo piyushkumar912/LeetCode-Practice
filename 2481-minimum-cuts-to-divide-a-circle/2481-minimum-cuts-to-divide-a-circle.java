7df3e7a7992200fab6025c7f49d5d41f726acb84
PIYUSH KUMAR <piyushrana7766@gmail.com>
2026-06-17 23:28:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (60.32%) - LeetHub

---
+++ 2481-minimum-cuts-to-divide-a-circle/2481-minimum-cuts-to-divide-a-circle.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfCuts(int n) {
+        if(n == 1){
+            return 0;
+        }
+        if(n % 2 == 0){
+            return n/2;
+        }
+        else{
+            return n;
+        }
+    }
+}
+
+
+// Time: O(1)
+//Space: O(1)
