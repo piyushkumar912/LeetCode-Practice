882063b9ed52b3d29fd229de546ad0f71ca91669
PIYUSH KUMAR <piyushrana7766@gmail.com>
2026-07-17 19:47:44 +0530

    Time: 1 ms (99.88%), Space: 44.5 MB (40.52%) - LeetHub

---
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String trimTrailingVowels(String s) {
+       int n = s.length();
+       int count = 0;
+       String vowels = "aeiou";
+       for(int i = n-1; i>=0; i--){
+        char ch = s.charAt(i);
+        if(vowels.indexOf(ch) != -1) count++;
+        else break;
+       }
+       return s.substring(0, n-count);
+       }
+    }
