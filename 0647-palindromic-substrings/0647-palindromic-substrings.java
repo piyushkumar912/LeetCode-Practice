ddb5f870844efe590b05a8f56c17089f638cd50f
PIYUSH KUMAR <piyushrana7766@gmail.com>
2026-09-18 20:10:07 +0530

    Time: 6 ms (62.51%), Space: 42.8 MB (68.43%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -3,6 +3,7 @@ class Solution {
         int n = s.length();
         int count = 0;
         for(int i = 0; i < n; i++){
+            //For ODD
             int low = i;
             int high = i;
             while(low >= 0 && high < n && s.charAt(low) == s.charAt(high)){
@@ -10,6 +11,7 @@ class Solution {
                 high++;
                 count++;
             }
+            // For EVEN
             int low1 = i;
             int high1 = i+1;
              while(low1 >= 0 && high1 < n && s.charAt(low1) == s.charAt(high1)){
